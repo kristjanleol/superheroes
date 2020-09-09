2475001c13b9152ad745a51c2fd134470937236d
kristjanleol <kristjan.leol@gmail.com>
2020-09-09 10:51:47 +0300

    superheroes and villains

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ console.log(superheroes.random());
 superheroes.all.forEach(hero => {
 
 });
-//console.log(superhero.all);
+//console.log(superhero.all);*/
+
+let supervillains = require('supervillains');
+
+//console.log(supervillains.all);
+console.log(supervillains.random());
+superheroes.all.forEach(hero => {
+
+});
+console.log(`${supervillains.random} fights ${superheroes.random}`);
+console.log(superheroes.random(), "fights" , supervillains.random());
